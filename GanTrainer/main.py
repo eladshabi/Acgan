d0bfa73d4e0751224c03b84ce6d060af1531d47d
Elad Shabi <elad.eladsh@gmail.com>
2019-01-23 10:44:10 +0200

    Update main.py

--- GanTrainer/main.py
+++ GanTrainer/main.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     # Get all the parameters for the training.
     training_time = int(sys.argv[1])
     batch_size = int(sys.argv[2])
-    root_path = os.getcwd()
+    root_path = os.path.dirname(os.path.abspath(__file__))
 
     # Create the Ac_gan model
     ac_gan = ACGAN(28, 28, 1, 10, 100)
