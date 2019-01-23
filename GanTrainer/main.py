067489bae03390e98d2bc61c843de46ca422c453
Elad Shabi <elad.eladsh@gmail.com>
2019-01-23 10:28:03 +0200

    Update main.py

--- GanTrainer/main.py
+++ GanTrainer/main.py
@@ -2,6 +2,7 @@ from GanTrainer import GanTrainer
 from GAN import ACGAN
 from GanUtils import *
 import sys
+import os
 
 
 if __name__ == "__main__":
@@ -9,12 +10,13 @@ if __name__ == "__main__":
     # Get all the parameters for the training.
     training_time = int(sys.argv[1])
     batch_size = int(sys.argv[2])
+    root_path = os.getcwd()
 
     # Create the Ac_gan model
     ac_gan = ACGAN(28, 28, 1, 10, 100)
 
     # Create the Gan trainer object and set the data loader function with the data folder path.
-    gan_trainer = GanTrainer(ac_gan, "", load_data)
+    gan_trainer = GanTrainer(ac_gan, root_path, load_data)
 
     # Train the model for specific time and custom batch size
     gan_trainer.train_gan_by_time(training_time, batch_size)
