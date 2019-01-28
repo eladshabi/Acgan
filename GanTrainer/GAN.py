7b592929ea160c678be9c8ce1540ca1db87de819
eladshabi <elad.eladsh@gmail.com>
2019-01-28 16:52:51 +0200

    Added tpu mode

--- GanTrainer/GAN.py
+++ GanTrainer/GAN.py
@@ -8,7 +8,7 @@ from tensorflow.keras.backend import set_floatx,set_epsilon
 
 from tensorflow.keras.layers import BatchNormalization, Layer
 from tensorflow.keras.initializers import Initializer
-from tensorflow.keras import initializers
+from tensorflow.keras import initializers, regularizers
 from keras.backend.tensorflow_backend import tf, _regular_normalize_batch_in_training
 
 from tensorflow.keras import backend as K
