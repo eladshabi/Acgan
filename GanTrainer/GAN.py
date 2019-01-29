9e6b3c40f50185fcf3f653981ec92b4d250a3db2
eladshabi <elad.eladsh@gmail.com>
2019-01-29 14:51:06 +0200

    Added tpu mode

--- GanTrainer/GAN.py
+++ GanTrainer/GAN.py
@@ -14,9 +14,47 @@ from keras.backend.tensorflow_backend import tf, _regular_normalize_batch_in_tra
 from tensorflow.keras import backend as K
 import tensorflow
 
-class DoubleGDOptimizer(tf.train.GradientDescentOptimizer):
-  def _valid_dtypes(self):
-    return set([tf.float16, tf.float32])
+
+class SGDMultiType(SGD):
+
+    def get_updates(self, loss, params):
+        grads = self.get_gradients(loss, params)
+        self.updates = [K.update_add(self.iterations, 1)]
+
+        lr = self.lr
+        if self.initial_decay > 0:
+            lr = lr * (1. / (1. + self.decay * K.cast(self.iterations,
+                                                      K.dtype(self.decay))))
+
+        # Adjusting learning rate for matching each weight type
+        learning_rates = [K.cast(lr, K.dtype(p)) for p in params]
+
+        # momentum
+        shapes = [K.int_shape(p) for p in params]
+
+        # adding custom types to moments
+        moments = [K.zeros(shape, dtype=K.dtype(p)) for p, shape in zip(params, shapes)]
+        self.weights = [self.iterations] + moments
+
+        # adjusting "self.momentum" value to weight types
+        momentums = [K.cast(self.momentum, K.dtype(p)) for p in params]
+
+        # using the typed learning rate and momentums
+        for p, g, m, lr, momentum in zip(params, grads, moments, learning_rates, momentums):
+            v = momentum * m - lr * g  # velocity
+            self.updates.append(K.update(m, v))
+
+            if self.nesterov:
+                new_p = p + momentum * v - lr * g
+            else:
+                new_p = p + v
+
+            # Apply constraints.
+            if getattr(p, 'constraint', None) is not None:
+                new_p = p.constraint(new_p)
+
+            self.updates.append(K.update(p, new_p))
+        return self.updates
 
 # custom initializers to force float32
 class Ones32(Initializer):
@@ -223,7 +261,7 @@ class ACGAN():
         self.latent_dim = latent
 
         #optimizer = Adam(0.0002, 0.5)
-        optimizer = DoubleGDOptimizer(0.001, 0.5)
+        optimizer = SGDMultiType(0.001, 0.5)
 
         losses = ['binary_crossentropy', 'sparse_categorical_crossentropy']
 
