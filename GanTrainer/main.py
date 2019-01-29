08983fb3986210f7b540430f6f28b70b9f02fd40
eladshabi <elad.eladsh@gmail.com>
2019-01-29 09:48:19 +0200

    Added tpu mode

--- GanTrainer/main.py
+++ GanTrainer/main.py
@@ -7,21 +7,6 @@ import os
 
 if __name__ == "__main__":
 
-#     # Get all the parameters for the training.
-#     training_time = int(sys.argv[1])
-#     batch_size = int(sys.argv[2])
-#     root_path = os.path.dirname(os.path.abspath(__file__))
-
-#     # Create the Ac_gan model
-#     ac_gan = ACGAN(28, 28, 1, 10, 100)
-
-#     # Create the Gan trainer object and set the data loader function with the data folder path.
-#     gan_trainer = GanTrainer(ac_gan, root_path, load_data)
-
-#     # Train the model for specific time and custom batch size
-#     gan_trainer.train_gan_by_time(training_time, batch_size)
-
-
     # Get all the parameters for the training.
     training_time = int(sys.argv[1])
     tpu = bool(sys.argv[2])
