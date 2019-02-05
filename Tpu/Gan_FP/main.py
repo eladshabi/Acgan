6dfb7766edd3a24903d934a517524b2ccf563cf9
eladshabi <elad.eladsh@gmail.com>
2019-02-05 17:36:30 +0200

    Added tpu mode

---
+++ Tpu/Gan_FP/main.py
@@ -0,0 +1,17 @@
+from gan import ACGAN
+from utils import show_all_variables
+import tensorflow as tf
+
+with tf.Session() as sess:
+
+    gan = ACGAN(sess, 2, 128, 100, "quick_draw", 'saved_model/', 'results/', 'logs')
+
+    # build graph
+    gan.build_model()
+
+    # show network architecture
+    show_all_variables()
+
+    # launch the graph in a session
+    gan.train()
+    print(" [*] Training finished!")
--- Tpu/Gan_FP/utils.py
+++ Tpu/Gan_FP/utils.py
@@ -148,26 +148,6 @@ def discrete_cmap(N, base_cmap=None):
     cmap_name = base.name + str(N)
     return base.from_list(cmap_name, color_list, N)
 
-def float32_variable_storage_getter(getter, name, shape=None, dtype=None,
-                                    initializer=None, regularizer=None,
-                                    trainable=True,
-                                    *args, **kwargs):
-    """Custom variable getter that forces trainable variables to be stored in
-    float32 precision and then casts them to the training precision.
-    """
-    print("in")
-    storage_dtype = tf.float32 if trainable else dtype
-
-    variable = getter(name, shape, dtype=storage_dtype,
-                      initializer=initializer, regularizer=regularizer,
-                      trainable=trainable,
-                      *args, **kwargs)
-    if trainable and dtype != tf.float32:
-        variable = tf.cast(variable, dtype)
-
-
-    print("res: ",variable)
-    return variable
 
 
 def get_name(name):
